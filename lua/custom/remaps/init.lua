e0c6bf259925d6a79775df4c0591e6ef13e16c56
Skqlixur <jmjmbo5@gmail.com>
2024-11-18 06:49:20 +0800

    Remove comment

--- lua/custom/remaps/init.lua
+++ lua/custom/remaps/init.lua
@@ -3,7 +3,9 @@ require 'custom.remaps.undotree'
 require 'custom.remaps.fugitive'
 require 'custom.remaps.live-server'
 
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>h', '<C-w>h', { desc = 'Focus window to the right' })
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open explorer' })
 
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
@@ -15,10 +17,3 @@ vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
 vim.keymap.set('x', '<leader>p', [["_dp]])
-
---[[
---
--- foo
--- bar
---
---]]
