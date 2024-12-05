ebafbfcae87036116ddf841cc9933d1ecd344ef3
skalixur <juanmateod04@gmail.com>
2024-12-05 19:35:09 +0800

    change live server open desc

--- lua/custom/remaps/live-server.lua
+++ lua/custom/remaps/live-server.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('n', '<leader>lo', vim.cmd.LiveServerStart, { desc = '[O]Start live server' })
+vim.keymap.set('n', '<leader>lo', vim.cmd.LiveServerStart, { desc = '[O]pen live server' })
 vim.keymap.set('n', '<leader>lp', vim.cmd.LiveServerStop, { desc = 'Sto[P] live server' })
