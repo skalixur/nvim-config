e17d5ed219427789f70d19d0a17a89945a6005e9
Skqlixur <jmjmbo5@gmail.com>
2025-01-25 02:51:20 +0800

    plug

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -67,4 +67,17 @@ return {
       },
     },
   },
+  {
+    'b0o/schemastore.nvim',
+    config = function()
+      require('lspconfig').jsonls.setup {
+        settings = {
+          json = {
+            schemas = require('schemastore').json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      }
+    end,
+  },
 }
