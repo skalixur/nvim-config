03a5f838160f3a64c4377b34d0a709687feb52de
skalixur <jmjmbo5@gmail.com>
2025-02-08 17:29:11 +0800

    Allow debian to use undofile

--- lua/options.lua
+++ lua/options.lua
@@ -12,7 +12,7 @@ vim.opt.expandtab = true
 vim.opt.wrap = true
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv 'HOME' .. '\\.vim\\undodir'
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 
 vim.opt.termguicolors = true
